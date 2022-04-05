4bdfd4a8ef7762d1cabceee1b6b7b3926914b0ad
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-04-05 10:39:09 -0300

    Started ej07

---
+++ Assets/Scripts/ej07.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej07 : MonoBehaviour
+{
+    //7. Realizá un programa que al ingresar por Inspector tres números enteros num1, num2 y
+    //num3, muestre el valor del mayor de todos.
+    //En caso de igualdad entre los tres imprimir &quot; Los números son iguales&quot;. Test1: num1 =
+    //12, num2 = 4, num3= 7. Resultado esperado: El mayor número es num1 Test2: num1
+    //= 2, num2 = 65, num3= 8. Resultado esperado: El mayor número es num2
+    public int num1;
+    public int num2;
+    public int num3;
+    // Use this for initialization
+    void Start () {
+		if (num1 == num2 & num1 == num3)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
